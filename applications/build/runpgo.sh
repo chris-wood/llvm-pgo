4960a22ac6c7993f64284e6e2fa0692a4024ecef
Julian Lettner <julian.lettner@gmail.com>
2013-10-16 16:47:22 -0700

    Add profiling options to pickup llvmprof.out when calling ./opt.

--- applications/build/runpgo.sh
+++ applications/build/runpgo.sh
@@ -8,4 +8,6 @@ path="$1"
 file_ext=$(basename "$path")
 filename="${file_ext%.*}"
 
-./opt -S -load PgoLoopUnroll.dylib -pgo-loop-unroll < "$file_ext" > "$filename-opt.s"
+profiling_options="-profile-loader -profile-metadata-loader -profile-verifier"
+
+./opt $profiling_options -S -load PgoLoopUnroll.dylib -pgo-loop-unroll < "$file_ext" > "$filename-pgo.s"
