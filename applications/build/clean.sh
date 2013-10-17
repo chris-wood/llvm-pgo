d22d6074a08da4cb7eaaa2e89c539b841f10cabb
Julian Lettner <julian.lettner@gmail.com>
2013-10-16 17:54:24 -0700

    Refine cleanup script.

--- applications/build/clean.sh
+++ applications/build/clean.sh
@@ -10,11 +10,13 @@ file_bc="$filename.bc"
 file_profile_bc="$filename.profile.bc"
 file_profile_s="$filename.profile.s"
 file_profile="$filename.profile"
+file_profile_pgo_s="$filename-pgo.s"
 
 rm $file_bc
 rm $file_profile_bc
 rm $file_profile_s
 rm $file_profile
+rm $file_profile_pgo_s
 rm "llvmprof.out"
 
 echo "Cleaned up for $filename".
