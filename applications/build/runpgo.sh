9d85912c5cf8cddbfe62c052f05186e756f9c6ca
Julian Lettner <julian.lettner@gmail.com>
2013-10-16 17:20:31 -0700

    Activate debug output for my pass (edit this file to enable debug output for other passes).

--- applications/build/runpgo.sh
+++ applications/build/runpgo.sh
@@ -10,4 +10,11 @@ filename="${file_ext%.*}"
 
 profiling_options="-profile-loader -profile-metadata-loader -profile-verifier"
 
-./opt $profiling_options -S -load PgoLoopUnroll.dylib -pgo-loop-unroll < "$file_ext" > "$filename-pgo.s"
+# enable all debug messages
+# debug_options="-debug"
+# 
+# enable debug messages only in the PgoLoopUnrollPass
+# corresponds to #define DEBUG_TYPE "pgo-loop-unroll"
+debug_options="-debug-only=pgo-loop-unroll"
+
+./opt -S $profiling_options $debug_options -load PgoLoopUnroll.dylib -pgo-loop-unroll < "$file_ext" > "$filename-pgo.s"
