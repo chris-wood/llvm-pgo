54b988aa44954d3ff8a342cb29d7f90acfb73200
Brian Belleville <brian.belleville@uci.edu>
2013-10-20 02:01:56 -0700

    Implement getInlineCost outside of the struct

--- llvm/lib/Transforms/PGO/PgoFunctionInline.cpp
+++ llvm/lib/Transforms/PGO/PgoFunctionInline.cpp
@@ -11,21 +11,15 @@ namespace {
   struct PgoFunctionInline : public Inliner {
     static char ID;
     PgoFunctionInline() : Inliner(ID) {}
-    
+
     virtual void getAnalysisUsage(AnalysisUsage &AU) const {
       AU.addRequired<ProfileInfo>();
       AU.addPreserved<ProfileInfo>();
       AU.addRequired<InlineCostAnalysis>();
       Inliner::getAnalysisUsage(AU);
     }
-    
-    virtual InlineCost getInlineCost(CallSite CS) {
-      ProfileInfo *PI = &getAnalysis<ProfileInfo>();
-      Function *F = CS.getCalledFunction();
-      if(F)
-	errs().write_escaped(F->getName()) << ": " << PI->getExecutionCount(F) << '\n';
-      return ICA->getInlineCost(CS, getInlineThreshold(CS));
-    }
+
+    virtual InlineCost getInlineCost(CallSite CS);
     virtual bool runOnSCC(CallGraphSCC &SCC);
   private:
     InlineCostAnalysis *ICA;
@@ -35,6 +29,14 @@ namespace {
 char PgoFunctionInline::ID = 0;
 
 static RegisterPass<PgoFunctionInline> X("pgo-function-inline", "Profile guided function inlining pass", false, false);
+
+InlineCost PgoFunctionInline::getInlineCost(CallSite CS){
+  ProfileInfo *PI = &getAnalysis<ProfileInfo>();
+  Function *F = CS.getCalledFunction();
+  if(F)
+    errs().write_escaped(F->getName()) << ": " << PI->getExecutionCount(F) << '\n';
+  return ICA->getInlineCost(CS, getInlineThreshold(CS));
+}
 
 bool PgoFunctionInline::runOnSCC(CallGraphSCC &SCC) {
   ICA = &getAnalysis<InlineCostAnalysis>();
