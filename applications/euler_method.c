07bf3713f6a5b6a508cc0bfe4af27627662c7151
Brian Belleville <brian.belleville@uci.edu>
2013-10-24 12:35:55 -0700

    Add loop to euler_method.c
    Compute the solution using euler's method in a tight loop.

--- applications/euler_method.c
+++ applications/euler_method.c
@@ -1,44 +1,53 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
- 
+
 typedef double (*deriv_f)(double, double);
 #define FMT " %7.3f"
- 
+
 void ivp_euler(deriv_f f, double y, int step, int end_t)
 {
-	int t = 0;
- 
-	printf(" Step %2d: ", (int)step);
-	do {
-		if (t % 10 == 0) printf(FMT, y);
-		y += step * f(t, y);
-	} while ((t += step) <= end_t);
-	printf("\n");
+  int t = 0;
+
+  printf(" Step %2d: ", (int)step);
+  do {
+    if (t % 10 == 0) printf(FMT, y);
+    y += step * f(t, y);
+  } while ((t += step) <= end_t);
+  printf("\n");
 }
- 
+
 void analytic()
 {
-	double t;
-	printf("    Time: ");
-	for (t = 0; t <= 100; t += 10) printf(" %7g", t);
-	printf("\nAnalytic: ");
- 
-	for (t = 0; t <= 100; t += 10)
-		printf(FMT, 20 + 80 * exp(-0.07 * t));
-	printf("\n");
+  double t;
+  printf("    Time: ");
+  for (t = 0; t <= 100; t += 10) printf(" %7g", t);
+  printf("\nAnalytic: ");
+
+  for (t = 0; t <= 100; t += 10)
+    printf(FMT, 20 + 80 * exp(-0.07 * t));
+  printf("\n");
 }
- 
+
 double cooling(double t, double temp)
 {
-	return -0.07 * (temp - 20);
+  return -0.07 * (temp - 20);
 }
- 
-int main()
+
+int run_benchmark()
 {
-	analytic();
-	ivp_euler(cooling, 100, 2, 100);
-	ivp_euler(cooling, 100, 5, 100);
-	ivp_euler(cooling, 100, 10, 100);
- 
-	return 0;
+  analytic();
+  ivp_euler(cooling, 100, 2, 100);
+  ivp_euler(cooling, 100, 5, 100);
+  ivp_euler(cooling, 100, 10, 100);
+
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  int n = atoi(argv[1]);
+  for (int i = 0; i < n; ++i)
+    run_benchmark();
+  return 0;
 }
