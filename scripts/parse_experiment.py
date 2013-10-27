9d606f863b48404f4a561f8946a082b40e05132c
chris-wood <christopherwood07@gmail.com>
2013-10-27 10:32:32 -0700

    Updating the scripts galore

--- scripts/parse_experiment.py
+++ scripts/parse_experiment.py
@@ -1,18 +1,54 @@
 import sys
+import os
 
 def usage():
     print >> sys.stderr, "usage: python parse_experiment.py list_of_program_prefixes"
 
 def main():
     print >> sys.stderr, "Starting to parse output..."
-    data = {} # map to list of lists
+    dataBucket = {} # map to list of lists
     fname = sys.argv[1]
     prefix_fnames = open(fname, 'r')
     for prefix in prefix_fnames:
         prefix = prefix.strip()
         print >> sys.stderr, "Parsing: " + str(prefix)
-        
-        # walk all files that start with the prefix... if they contain .mod then add time in each file to the modified bucket, otherwise add time to the regular bucket
+
+        # Add this prefix to the data bucket
+        dataBucket[prefix] = []
+        dataBucket[prefix].append([]) # list for modified data time
+        dataBucket[prefix].append([]) # lost for unmodified data time
+        for dirpath, dnames, fnames in os.walk("."):
+            for f in fnames:
+                if f.startswith(prefix):
+                    full = os.path.join(dirpath, f)
+
+                    # Extract time
+                    ftimef = open(full, 'r')
+                    size
+                    time = 0
+                    for l in ftimef:
+                        if "TIME:" in l:
+                            data = l.split(":").split(",")
+                            size = data[0]
+                            time = float(data[1])
+                            break
+                    if f.contains(".mod."):
+                        dataBucket[prefix][0].append((size, time))
+                    else:
+                        dataBucket[prefix][1].append((size, time))
+
+        # Generate CSV output 
+        minSamples = min(len(dataBucket[prefix][0]), len(dataBucket[prefix][1]))
+        csv = ""
+        for i in range(len(dataBucket[prefix][0])):
+            for j in range(len(dataBucket[prefix][1])):
+                if (dataBucket[prefix][0][i][0] == dataBucket[prefix][1][j][0]): # if these times correspond to the same input size...
+                    size = dataBucket[prefix][0][i][0]
+                    time1 = dataBucket[prefix][0][i][1]
+                    time2 = dataBucket[prefix][0][j][1]
+                    csv = csv + str(size) + "," + str(time1) + "," + str(time2) + "\n"
+        print(prefix + " CSV"):
+        print(csv)
 
 if __name__ == "__main__":
     main()
--- scripts/time.pl
+++ scripts/time.pl
@@ -6,4 +6,4 @@ my $start = time;
 system("./$ARGV[0] $ARGV[1] > $ARGV[0].out");
 my $end = time;
 my $run_time = $end - $start;
-print "Elapsed time: $run_time\n";
+print "TIME: $ARGV[1],$run_time\n";
