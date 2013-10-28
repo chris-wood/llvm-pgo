65297a197e3dcec7c45c3e247cd38a2dff9b5167
chris-wood <christopherwood07@gmail.com>
2013-10-28 13:10:10 -0700

    Updated scripts for testing purposes

--- scripts/run_basicblock.sh
+++ scripts/run_basicblock.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 echo Running program: $1
-var=$1
-count=$2
-for i in ${@:3}
+compilerArgs=$1
+var=$2
+count=$3
+for i in ${@:4}
 do
 	echo ----------------------------------------
 	echo *** Size: $i
 	echo ----------------------------------------
-    ./clang -03 -emit-llvm $var.c -c -o $var.bc
-    ./opt -insert-edge-profiling $var.bc -o $var.profile.bc
-	./llc $var.profile.bc -o $var.profile.s
-	./clang -o $var.profile $var.profile.s ../lib/libprofile_rt.so
+    ./clang -03 -emit-llvm $var.c -c -o $var.bc $compilerArgs 2>&1>> $var.$i.build
+    ./opt -insert-edge-profiling $var.bc -o $var.profile.bc 2>&1>> $var.$i.build
+	./llc $var.profile.bc -o $var.profile.s 2>&1>> $var.$i.build
+	./clang -o $var.profile $var.profile.s ../lib/libprofile_rt.so $compilerArgs 2>&1>> $var.$i.build
 
 	# Gather the profile data and output, for peace of mind
-	./$var.profile $i > $var.profile.out
-	./llvm-prof $var.profile.bc
+	./$var.profile $i > $var.profile.out 2>&1>> $var.$i.dump
+	./llvm-prof $var.profile.bc 2>&1>> $var.$i.dump
 
 	# Now do the block placement and gather the results...
-	./opt -profile-loader -block-placement $var.bc
-	./llc $var.bc -o $var.mod.s
-	./clang -o $var.mod $var.mod.s ../lib/libprofile_rt.so
-	./$var.mod $i > $var.mod.out
-	./clang -o $var $var.c
+	./opt -profile-loader -block-placement $var.bc 2>&1>> $var.$i.optbuild
+	./llc $var.bc -o $var.mod.s 2>&1>> $var.$i.optbuild
+	./clang -o $var.mod $var.mod.s ../lib/libprofile_rt.so $compilerArgs 2>&1>> $var.$i.optbuild
+	./$var.mod $i > $var.mod.out 2>&1>> $var.$i.dump
+	./clang -o $var $var.c $compilerArgs 2>&1>> $var.$i.optbuild
 
 	# Now run the program with the time script and save the output
 	COUNTER=0
