4a6af63779d463efe83e1ecc1265d68a6abce845
chris-wood <christopherwood07@gmail.com>
2013-10-28 13:14:41 -0700

    fixed output redirection

--- scripts/run_basicblock.sh
+++ scripts/run_basicblock.sh
@@ -8,21 +8,21 @@ do
 	echo ----------------------------------------
 	echo *** Size: $i
 	echo ----------------------------------------
-    ./clang -03 -emit-llvm $var.c -c -o $var.bc $compilerArgs 2>&1>> $var.$i.build
-    ./opt -insert-edge-profiling $var.bc -o $var.profile.bc 2>&1>> $var.$i.build
-	./llc $var.profile.bc -o $var.profile.s 2>&1>> $var.$i.build
-	./clang -o $var.profile $var.profile.s ../lib/libprofile_rt.so $compilerArgs 2>&1>> $var.$i.build
+    ./clang -03 -emit-llvm $var.c -c -o $var.bc $compilerArgs >> $var.$i.build 2>&1
+    ./opt -insert-edge-profiling $var.bc -o $var.profile.bc >> $var.$i.build 2>&1
+	./llc $var.profile.bc -o $var.profile.s >> $var.$i.build 2>&1
+	./clang -o $var.profile $var.profile.s ../lib/libprofile_rt.so $compilerArgs >> $var.$i.build 2>&1
 
 	# Gather the profile data and output, for peace of mind
-	./$var.profile $i > $var.profile.out 2>&1>> $var.$i.dump
-	./llvm-prof $var.profile.bc 2>&1>> $var.$i.dump
+	./$var.profile $i > $var.profile.out >> $var.$i.dump 2>&1
+	./llvm-prof $var.profile.bc >> $var.$i.dump 2>&1
 
 	# Now do the block placement and gather the results...
-	./opt -profile-loader -block-placement $var.bc 2>&1>> $var.$i.optbuild
-	./llc $var.bc -o $var.mod.s 2>&1>> $var.$i.optbuild
-	./clang -o $var.mod $var.mod.s ../lib/libprofile_rt.so $compilerArgs 2>&1>> $var.$i.optbuild
-	./$var.mod $i > $var.mod.out 2>&1>> $var.$i.dump
-	./clang -o $var $var.c $compilerArgs 2>&1>> $var.$i.optbuild
+	./opt -profile-loader -block-placement $var.bc >> $var.$i.optbuild 2>&1
+	./llc $var.bc -o $var.mod.s >> $var.$i.optbuild 2>&1
+	./clang -o $var.mod $var.mod.s ../lib/libprofile_rt.so $compilerArgs >> $var.$i.optbuild 2>&1
+	./$var.mod $i > $var.mod.out >> $var.$i.dump 2>&1
+	./clang -o $var $var.c $compilerArgs >> $var.$i.optbuild 2>&1
 
 	# Now run the program with the time script and save the output
 	COUNTER=0
