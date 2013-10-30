acf6391a3add91769716a40a6346decb77c81bad
caw <christopherwood07@gmail.com>
2013-10-29 19:39:51 -0700

    Updated data and parsing script - we need to tweak the parparameters to get these things to run longer

--- data/parse_experiment.py
+++ data/parse_experiment.py
@@ -11,44 +11,96 @@ def main():
     prefix_fnames = open(fname, 'r')
     for prefix in prefix_fnames:
         prefix = prefix.strip()
-        print >> sys.stderr, "Parsing: " + str(prefix)
+        if len(prefix) > 0:
+            print >> sys.stderr, "Parsing: " + str(prefix)
 
-        # Add this prefix to the data bucket
-        dataBucket[prefix] = []
-        dataBucket[prefix].append([]) # list for modified data time
-        dataBucket[prefix].append([]) # lost for unmodified data time
-        for dirpath, dnames, fnames in os.walk("."):
-            for f in fnames:
-                if f.startswith(prefix):
-                    full = os.path.join(dirpath, f)
+            # Add this prefix to the data bucket
+            dataBucket[prefix] = []
+            dataBucket[prefix].append([]) # list for modified data time
+            dataBucket[prefix].append([]) # list for unmodified data time
 
-                    # Extract time
-                    ftimef = open(full, 'r')
-                    size = 0
-                    time = 0
-                    for l in ftimef:
-                        if "TIME:" in l:
-                            data = l.split(":")[1].split(",")
-                            size = data[0]
-                            time = float(data[1])
-                            break
-                    if ".mod." in f:
-                        dataBucket[prefix][0].append((size, time))
-                    else:
-                        dataBucket[prefix][1].append((size, time))
+            dataBucket[prefix].append([]) # first modified
+            dataBucket[prefix].append([]) # first unmodified
 
-        # Generate CSV output 
-        minSamples = min(len(dataBucket[prefix][0]), len(dataBucket[prefix][1]))
-        csv = ""
-        for i in range(len(dataBucket[prefix][0])):
-            for j in range(len(dataBucket[prefix][1])):
-                if (dataBucket[prefix][0][i][0] == dataBucket[prefix][1][j][0]): # if these times correspond to the same input size...
-                    size = dataBucket[prefix][0][i][0]
-                    time1 = dataBucket[prefix][0][i][1]
-                    time2 = dataBucket[prefix][1][j][1]
-                    csv = csv + str(size) + "," + str(time1) + "," + str(time2) + "\n"
-        print(prefix + " CSV")
-        print(csv)
+            dataBucket[prefix].append([]) # last modified
+            dataBucket[prefix].append([]) # last unmodified
+            for dirpath, dnames, fnames in os.walk("."):
+                for f in fnames:
+                    if f.startswith(prefix):
+                        full = os.path.join(dirpath, f)
+
+                        # Extract time
+                        ftimef = open(full, 'r')
+                        size = 0
+                        time = 0
+                        for l in ftimef:
+                            if "TIME:" in l:
+                                data = l.split(":")[1].split(",")
+                                size = int(data[0])
+                                time = float(data[1])
+                                break
+                        if ".first." and ".mod" in f:
+                            dataBucket[prefix][2].append((size, time))
+                        elif ".first." in f:
+                            dataBucket[prefix][3].append((size, time))
+                        elif ".last." and ".mod" in f:
+                            dataBucket[prefix][4].append((size, time))
+                        elif ".last." in f:
+                            dataBucket[prefix][5].append((size, time))
+                        elif ".mod." in f:
+                            dataBucket[prefix][0].append((size, time))
+                        else:
+                            dataBucket[prefix][1].append((size, time))
+
+            # Generate CSV output for same input comparison
+            minSamples = min(len(dataBucket[prefix][0]), len(dataBucket[prefix][1]))
+            csv = "size,optimized,unoptimized\n"
+            for i in range(len(dataBucket[prefix][0])):
+                for j in range(len(dataBucket[prefix][1])):
+                    if (dataBucket[prefix][0][i][0] == dataBucket[prefix][1][j][0]): # if these times correspond to the same input size...
+                        size = dataBucket[prefix][0][i][0]
+                        time1 = dataBucket[prefix][0][i][1]
+                        time2 = dataBucket[prefix][1][j][1]
+                        csv = csv + str(size) + "," + str(time1) + "," + str(time2) + "\n"
+            
+            # Write the data to a CSV file
+            csvf = open(prefix + ".data.csv", 'w')
+            csvf.write(csv + "\n")
+            csvf.close()
+
+            ###### FIRST
+
+            # Generate CSV output for first comparison
+            csv = "size,optimized,unoptimized"
+            for i in range(len(dataBucket[prefix][2])):
+                for j in range(len(dataBucket[prefix][3])):
+                    if (dataBucket[prefix][2][i][0] == dataBucket[prefix][3][j][0]): # if these times correspond to the same input size...
+                        size = dataBucket[prefix][2][i][0]
+                        time1 = dataBucket[prefix][2][i][1]
+                        time2 = dataBucket[prefix][3][j][1]
+                        csv = csv + str(size) + "," + str(time1) + "," + str(time2) + "\n"
+            
+            # Write the data to a CSV file
+            csvf = open(prefix + ".first.data.csv", 'w')
+            csvf.write(csv + "\n")
+            csvf.close()
+
+            ###### LAST
+
+            # Generate CSV output for first comparison
+            csv = "size,optimized,unoptimized"
+            for i in range(len(dataBucket[prefix][4])):
+                for j in range(len(dataBucket[prefix][5])):
+                    if (dataBucket[prefix][4][i][0] == dataBucket[prefix][5][j][0]): # if these times correspond to the same input size...
+                        size = dataBucket[prefix][4][i][0]
+                        time1 = dataBucket[prefix][4][i][1]
+                        time2 = dataBucket[prefix][5][j][1]
+                        csv = csv + str(size) + "," + str(time1) + "," + str(time2) + "\n"
+            
+            # Write the data to a CSV file
+            csvf = open(prefix + ".last.data.csv", 'w')
+            csvf.write(csv + "\n")
+            csvf.close()
 
 if __name__ == "__main__":
     main()
--- scripts/parse_experiment.py
+++ scripts/parse_experiment.py
@@ -5,19 +5,25 @@ def usage():
     print >> sys.stderr, "usage: python parse_experiment.py list_of_program_prefixes"
 
 def main():
-    print >> sys.stderr, "Starting to parse output..."
     dataBucket = {} # map to list of lists
     fname = sys.argv[1]
     prefix_fnames = open(fname, 'r')
     for prefix in prefix_fnames:
         prefix = prefix.strip()
         if len(prefix) > 0:
-            print >> sys.stderr, "Parsing: " + str(prefix)
 
             # Add this prefix to the data bucket
             dataBucket[prefix] = []
             dataBucket[prefix].append([]) # list for modified data time
-            dataBucket[prefix].append([]) # lost for unmodified data time
+            dataBucket[prefix].append([]) # list for unmodified data time
+
+            dataBucket[prefix].append([]) # first modified
+            dataBucket[prefix].append([]) # first unmodified
+
+            dataBucket[prefix].append([]) # last modified
+            dataBucket[prefix].append([]) # last unmodified
+            print >> sys.stderr, "Parsing prefix: " + str(prefix)
+
             for dirpath, dnames, fnames in os.walk("."):
                 for f in fnames:
                     if f.startswith(prefix):
@@ -33,24 +39,134 @@ def main():
                                 size = int(data[0])
                                 time = float(data[1])
                                 break
-                        if ".mod." in f:
+                        if ".first." in f and ".mod" in f:
+                            dataBucket[prefix][2].append((size, time))
+                        elif ".first." in f:
+                            dataBucket[prefix][3].append((size, time))
+                        elif ".last." in f and ".mod" in f:
+                            dataBucket[prefix][4].append((size, time))
+                        elif ".last." in f:
+                            dataBucket[prefix][5].append((size, time))
+                        elif ".mod." in f:
                             dataBucket[prefix][0].append((size, time))
                         else:
                             dataBucket[prefix][1].append((size, time))
 
-            # Generate CSV output 
-            minSamples = min(len(dataBucket[prefix][0]), len(dataBucket[prefix][1]))
+            # Generate CSV output for first comparison
             csv = "size,optimized,unoptimized\n"
+            total1 = {}
+            count1 = {}
             for i in range(len(dataBucket[prefix][0])):
-                for j in range(len(dataBucket[prefix][1])):
-                    if (dataBucket[prefix][0][i][0] == dataBucket[prefix][1][j][0]): # if these times correspond to the same input size...
-                        size = dataBucket[prefix][0][i][0]
-                        time1 = dataBucket[prefix][0][i][1]
-                        time2 = dataBucket[prefix][1][j][1]
-                        csv = csv + str(size) + "," + str(time1) + "," + str(time2) + "\n"
+                size = dataBucket[prefix][0][i][0]
+                if not (size in total1):
+                    total1[size] = 0.0
+                    count1[size] = 0
+                total1[size] = total1[size] + dataBucket[prefix][0][i][1]
+                count1[size] = count1[size] + 1
+            avg1 = {}
+            for i in total1:
+                avg1[i] = total1[i] / count1[i]
+
+            total2 = {}
+            count2 = {}
+            for i in range(len(dataBucket[prefix][1])):
+                size = dataBucket[prefix][1][i][0]
+                if not (size in total2):
+                    total2[size] = 0.0
+                    count2[size] = 0
+                total2[size] = total2[size] + dataBucket[prefix][1][i][1]
+                count2[size] = count2[size] + 1
+            avg2 = {}
+            for i in total2:
+                avg2[i] = total2[i] / count2[i]
+
+            for i in avg1:
+                for j in avg2:
+                    if i == j:
+                        csv = csv + str(i) + "," + str(avg1[i]) + "," + str(avg2[i]) + "\n"
             
             # Write the data to a CSV file
             csvf = open(prefix + ".data.csv", 'w')
+            csvf.write(csv + "\n")
+            csvf.close()
+
+            ###### FIRST
+
+            # Generate CSV output for first comparison
+            csv = "size,optimized,unoptimized\n"
+            total1 = {}
+            count1 = {}
+            for i in range(len(dataBucket[prefix][2])):
+                size = dataBucket[prefix][2][i][0]
+                if not (size in total1):
+                    total1[size] = 0.0
+                    count1[size] = 0
+                total1[size] = total1[size] + dataBucket[prefix][2][i][1]
+                count1[size] = count1[size] + 1
+            avg1 = {}
+            for i in total1:
+                avg1[i] = total1[i] / count1[i]
+
+            total2 = {}
+            count2 = {}
+            for i in range(len(dataBucket[prefix][3])):
+                size = dataBucket[prefix][3][i][0]
+                if not (size in total2):
+                    total2[size] = 0.0
+                    count2[size] = 0
+                total2[size] = total2[size] + dataBucket[prefix][3][i][1]
+                count2[size] = count2[size] + 1
+            avg2 = {}
+            for i in total2:
+                avg2[i] = total2[i] / count2[i]
+
+            for i in avg1:
+                for j in avg2:
+                    if i == j:
+                        csv = csv + str(i) + "," + str(avg1[i]) + "," + str(avg2[i]) + "\n"
+            
+            # Write the data to a CSV file
+            csvf = open(prefix + ".first.data.csv", 'w')
+            csvf.write(csv + "\n")
+            csvf.close()
+
+            ###### LAST
+
+            # Generate CSV output for first comparison
+            csv = "size,optimized,unoptimized\n"
+            total1 = {}
+            count1 = {}
+            for i in range(len(dataBucket[prefix][4])):
+                size = dataBucket[prefix][4][i][0]
+                if not (size in total1):
+                    total1[size] = 0.0
+                    count1[size] = 0
+                total1[size] = total1[size] + dataBucket[prefix][4][i][1]
+                count1[size] = count1[size] + 1
+            avg1 = {}
+            for i in total1:
+                avg1[i] = total1[i] / count1[i]
+
+            total2 = {}
+            count2 = {}
+            for i in range(len(dataBucket[prefix][5])):
+                size = dataBucket[prefix][5][i][0]
+                if not (size in total2):
+                    total2[size] = 0.0
+                    count2[size] = 0
+                total2[size] = total2[size] + dataBucket[prefix][5][i][1]
+                count2[size] = count2[size] + 1
+            avg2 = {}
+            for i in total2:
+                avg2[i] = total2[i] / count2[i]
+
+            for i in avg1:
+                for j in avg2:
+                    if i == j:
+                        csv = csv + str(i) + "," + str(avg1[i]) + "," + str(avg2[i]) + "\n"
+            
+            # Write the data to a CSV file
+            csvf = open(prefix + ".last.data.csv", 'w')
             csvf.write(csv + "\n")
             csvf.close()
 
