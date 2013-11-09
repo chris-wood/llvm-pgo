73f48457351d9219d452a4ab002aaf586adfaee7
Julian Lettner <julian.lettner@gmail.com>
2013-11-08 16:05:20 -0800

    Revert "Added PgoLoopUnroll and first implementation sub."
    This reverts commit 78328b5db5e3bf21835d1f141462423d2d94dac8.

--- llvm/include/llvm/Transforms/Utils/UnrollLoop.h
+++ llvm/include/llvm/Transforms/Utils/UnrollLoop.h
@@ -28,8 +28,6 @@ bool UnrollLoop(Loop *L, unsigned Count, unsigned TripCount, bool AllowRuntime,
 bool UnrollRuntimeLoopProlog(Loop *L, unsigned Count, LoopInfo *LI,
                              LPPassManager* LPM);
 
-bool PgoUnrollLoop(Loop *L, unsigned Count, LoopInfo *LI, LPPassManager *LPM);
-
 }
 
 #endif
